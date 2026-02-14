a9fee9a7ef35ded1572a48796b5e163c827fb312
Vinayak Tiwari <vinayaktiwari00326@gmail.com>
2026-02-14 16:07:44 +0530

    Create 02_primitiveDataType.java

---
+++ 02_primitiveDataType.java
@@ -0,0 +1,14 @@
+class primitiveDataType {
+    public static void main(String[] args){
+        int num = 43;
+        byte by = 127;
+        short sh = 558;
+        long l = 3434l;
+
+        float f = 5.6f;
+        double d = 4.5;
+
+        char c = 'k';
+        boolean b = true;
+    }
+}
